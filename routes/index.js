818dcbebdf6a59e90ebf71db154efcac8d129355
Van Huy <1712056@student.hcmus.edu.vn>
2019-11-02 18:59:04 +0700

    add full routing for webpage

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,30 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
 });
-
+router.get('/index.html', function(req, res, next) {
+  res.render('index');
+});
+router.get('/checkout.html', function(req, res, next) {
+  res.render('checkout');
+});
+router.get('/contact.html', function(req, res, next) {
+  res.render('contact');
+});
+router.get('/error.html', function(req, res, next) {
+  res.render('error');
+});
+router.get('/products.html', function(req, res, next) {
+  res.render('products');
+});
+router.get('/register.html', function(req, res, next) {
+  res.render('register');
+});
+router.get('/signup.html', function(req, res, next) {
+  res.render('signup');
+});
+router.get('/single.html', function(req, res, next) {
+  res.render('single');
+});
 module.exports = router;
