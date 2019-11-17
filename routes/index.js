c7d28c581ec0e63a2d8a1aa82137a8bdee149eb0
Van Huy <1712056@student.hcmus.edu.vn>
2019-11-17 23:36:41 +0700

    add detail product for web user

--- routes/index.js
+++ routes/index.js
@@ -19,13 +19,14 @@ router.get('/error.html', function(req, res, next) {
 });
 var pg = require('pg');
 var config = {
-  user: 'postgres',
-  database: 'users-nair',
-  password: '12345',
-  host: 'localhost',
+  user: 'lwmtvkbdcppemz',
+  database: 'd2ftd0sald3l3t',
+  password: '8e53568db97c2950fa93af795d1dac6c2581239b483f0950bf86776af685d482',
+  host: 'ec2-107-21-111-24.compute-1.amazonaws.com',
   port: 5432,
   max: 10,
   idleTimeoutMillis: 30000,
+  ssl:true,
 };
 var pool = new pg.Pool(config);
 router.get('/sanpham',function(req, res, next){
@@ -51,7 +52,46 @@ router.get('/dangky', function(req, res, next) {
 router.get('/dangnhap', function(req, res, next) {
   res.render('signup');
 });
-router.get('/chitiet', function(req, res, next) {
-  res.render('single');
+router.get('/chitiet:id',function(req, res, next){
+
+  var id = req.params.id;
+  parseInt(id);
+  if(id==1 || id==9 || id==11)
+  {
+    id=2;
+  }
+  else if(id==2 || id==6)
+  {
+    id=1;
+  }
+  else if(id==3)
+  {
+    id=5;
+  }
+  else if(id==4 || id==12 ||id==11||id==13)
+  {
+    id=4;
+  }
+  else if(id==5)
+  {
+    id=3;
+  }
+  else if(id==7)
+    {
+      id=6;
+    }
+  pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack)
+    }
+    client.query('SELECT * FROM "Chitiet" WHERE "id"='+ id, (err, result) => {
+      release()
+      if (err) {
+        return console.error('Error executing query', err.stack)
+      }
+      console.log();
+      res.render("single", {data:result.rows});
+    })
+  })
 });
 module.exports = router;
